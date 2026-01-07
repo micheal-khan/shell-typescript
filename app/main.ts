18ed104e274aa44d159ea0281d17c4d0cc9f6ba4
Micheal <khanzaidan786@gmail.com>
2026-01-07 16:18:24 +0530

    The ~ (tilde) character

--- app/main.ts
+++ app/main.ts
@@ -64,7 +64,7 @@ rl.on("line", (line) => {
         if (args.toString() === "~") {
           process.chdir(process.env.HOME || process.env.USERPROFILE || "/");
         } else {
-          process.chdir(args.toString()); // Changes the working directory of the Node.js 
+          process.chdir(args.toString()); // Changes the working directory of the Node.js process
         }
       } catch (err) {
         console.error(`${args}: No such file or directory`);
